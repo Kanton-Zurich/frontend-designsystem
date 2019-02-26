6baba3829ff343d3f7e99b720460763978ba96a6
Marcel Bührig <marcel.buehrig@zeix.com>
2019-02-26 10:32:22 +0100

    Added linting ignore rules, added dangled commas in webpack.config, fixed preview/main.scss

--- webpack.config.js
+++ webpack.config.js
@@ -38,9 +38,9 @@ module.exports = [
         {
           test: /\.tsx?$/,
           use: 'ts-loader',
-          exclude: /node_modules/
-        }
-      ]
+          exclude: /node_modules/,
+        },
+      ],
     },
     resolve: {
       extensions: [ '.tsx', '.ts', '.js' ]
