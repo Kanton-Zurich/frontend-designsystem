33ba7c859b856190a774e02ba365f181ac43ed4a
Robert Wüest <robert.wueest@one-inside.com>
2019-04-10 11:42:53 +0000

    Revert "Change deployment paths (pull request #24)"
    This reverts pull request #24.
    
    > Change deployment paths

--- webpack.config.js
+++ webpack.config.js
@@ -49,6 +49,6 @@ module.exports = [
       path: path.resolve('./dist/preview/assets/js'),
       filename: `[name].js`,
     },
-    mode: env.dev ? 'development' : 'production',
+    mode: 'development',
   },
 ];
