80d312a603a8f5244c617a6534fcaccdc93d36eb
Esther Brunner <esther.brunner@zeix.com>
2019-04-18 18:56:49 +0200

    CZHDEV-283 Demo Page [wip]

---
+++ src/pages/single/single.data.js
@@ -0,0 +1,21 @@
+const _ = require('lodash');
+const defaultData = require('../../data/default.data.js');
+const dataHelper = require('@unic/estatico-data');
+
+const data = _.merge({}, defaultData, {
+  meta: {
+    title: 'Einzelseite',
+    jira: 'CZHDEV-283',
+    content: dataHelper.getFileContent('single.hbs'),
+    documentation: dataHelper.getDocumentation('single.md'),
+  },
+  props: {
+    title: 'Einzelseite',
+    text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    modules: {
+      // xy: require('../../modules/xy/xy.data.js').props
+    },
+  },
+});
+
+module.exports = data;
