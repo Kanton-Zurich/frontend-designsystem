f2da1e0a640dca1b3ae25ab712d39d77d2e4911a
Esther Brunner <esther.brunner@zeix.com>
2019-04-23 19:16:14 +0200

    CZHDEV-292 Implemented skiplinks module

---
+++ src/modules/skiplinks/skiplinks.data.js
@@ -0,0 +1,72 @@
+const _ = require('lodash');
+const dataHelper = require('@unic/estatico-data');
+const { handlebars } = require('@unic/estatico-handlebars');
+const defaultData = require('../../data/default.data.js');
+
+const template = dataHelper.getFileContent('skiplinks.hbs');
+const data = _.merge({}, defaultData, {
+  meta: {
+    title: 'Skiplinks',
+    className: 'Skiplinks',
+    jira: 'CZHDEV-292',
+    documentation: dataHelper.getDocumentation('skiplinks.md'),
+  },
+  props: {
+    title: 'Sprunglinks',
+    links: [
+      {
+        href: '/',
+        accesskey: 0,
+        label: 'Startseite',
+      }, {
+        href: '#navigation',
+        accesskey: 1,
+        label: 'Navigation',
+      }, {
+        href: '#content',
+        accesskey: 2,
+        label: 'Inhalt',
+      }, {
+        href: '/kontakt.html',
+        accesskey: 3,
+        label: 'Kontakt',
+      }, {
+        href: '/suche.html',
+        accesskey: 4,
+        label: 'Suche',
+      }, {
+        href: '#contentinfo',
+        accesskey: 5,
+        label: 'Fussbereich',
+      },
+    ],
+  },
+});
+const variants = _.mapValues({
+  default: {
+    meta: {
+      title: 'Standard',
+      desc: 'Standard-Implementation',
+    },
+  },
+}, (variant) => {
+  const variantProps = _.merge({}, data, variant).props;
+  const compiledVariant = () => handlebars.compile(template)(variantProps);
+  const variantData = _.merge({}, data, variant, {
+    meta: {
+      demo: compiledVariant,
+
+      code: {
+        handlebars: dataHelper.getFormattedHandlebars(template),
+        html: dataHelper.getFormattedHtml(compiledVariant()),
+        data: dataHelper.getFormattedJson(variantProps),
+      },
+    },
+  });
+
+  return variantData;
+});
+
+data.variants = variants;
+
+module.exports = data;
--- src/pages/single/single.data.js
+++ src/pages/single/single.data.js
@@ -1,5 +1,7 @@
 const _ = require('lodash');
 const defaultData = require('../../data/default.data.js');
+const skiplinksData = require('../../modules/skiplinks/skiplinks.data.js');
+const richtextData = require('../../modules/richtext/richtext.data.js');
 const dataHelper = require('@unic/estatico-data');
 
 const data = _.merge({}, defaultData, {
@@ -14,6 +16,8 @@ const data = _.merge({}, defaultData, {
     text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
     modules: {
       // xy: require('../../modules/xy/xy.data.js').props
+      skiplinks: skiplinksData.props,
+      richtext: richtextData.props,
     },
   },
 });
