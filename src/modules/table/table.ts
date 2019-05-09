40a53fdf1e45a266b6e4d06c3d5715629cef928d
Marcel Bührig <marcel.buehrig@zeix.com>
2019-05-09 15:17:46 +0200

    Added shades to tables with fixed column

--- src/modules/table/table.ts
+++ src/modules/table/table.ts
@@ -4,6 +4,7 @@
  * @author
  * @copyright
  */
+import WindowEventListener from '../../assets/js/helpers/events';
 import Module from '../../assets/js/helpers/module';
 // import namespace from '../../assets/js/helpers/namespace';
 
@@ -23,16 +24,26 @@ class Table extends Module {
       firstColumnFixed: string,
       sortable: string,
       cloned: string,
+      shadeRight: string,
+      shadeLeft: string,
     },
   };
 
   public data: {
+    shades: {
+      left: any,
+      right: any,
+    },
     clonedTable: any,
-  }
+  };
 
   constructor($element: any, data: Object, options: Object) {
     const defaultData = {
       clonedTable: null,
+      shades: {
+        left: null,
+        right: null,
+      },
     };
     const defaultOptions = {
       domSelectors: {
@@ -46,6 +57,8 @@ class Table extends Module {
         firstColumnFixed: 'mdl-table--first-column-fixed',
         sortable: 'mdl-table--sortable',
         cloned: 'mdl-table__table--cloned',
+        shadeRight: 'mdl-table--shade-right',
+        shadeLeft: 'mdl-table--shade-left',
       },
     };
 
@@ -67,19 +80,10 @@ class Table extends Module {
   }
 
   /**
-   * Initialisation of variables, which point to DOM elements
-   */
-  initUi() {
-    // DOM element pointers
-    this.ui.table = this.ui.element.querySelector(this.options.domSelectors.table);
-    this.ui.scrollArea = this.ui.element.querySelector(this.options.domSelectors.scrollArea);
-  }
-
-  /**
    * Event listeners initialisation
    */
   initEventListeners() {
-    // Event listeners
+    (<any>WindowEventListener).addDebouncedResizeListener(this.setShades.bind(this));
   }
 
   /**
@@ -91,6 +95,86 @@ class Table extends Module {
     this.data.clonedTable.removeAttribute('data-table');
     this.data.clonedTable.setAttribute('aria-hidden', 'true');
     this.ui.scrollArea.append(this.data.clonedTable);
+  }
+
+  /**
+   * Setting up the shades, and the according event listeners
+   *
+   * @memberof Table
+   */
+  setupShades() {
+    this.ui.scrollAreaWrapper.addEventListener('scroll', this.setShades.bind(this));
+
+    this.createShadeNodes();
+    this.setLeftOffset();
+    this.setShades();
+  }
+
+  /**
+   * Creating the shade nodes as pseudo elements can't be styled via js
+   *
+   * @memberof Table
+   */
+  createShadeNodes() {
+    const leftShade = document.createElement('span');
+    const rightShade = document.createElement('span');
+
+    leftShade.setAttribute('aria-hidden', 'true');
+    rightShade.setAttribute('aria-hidden', 'true');
+
+    leftShade.classList.add('mdl-table__shade-left');
+    rightShade.classList.add('mdl-table__shade-right');
+
+    this.ui.scrollArea.appendChild(leftShade);
+    this.ui.scrollArea.appendChild(rightShade);
+
+    this.data.shades = {
+      left: leftShade,
+      right: rightShade,
+    };
+  }
+
+  /**
+   * Setting the offset for the left shade, as the first column is fixed
+   *
+   * @memberof Table
+   */
+  setLeftOffset() {
+    const firstThRow = this.ui.table.querySelector('th[scope="row"]');
+
+    this.data.shades.left.style.left = `${firstThRow.getBoundingClientRect().width}px`;
+  }
+
+  /**
+   * Getting the scroll percentage and setting the shades classes accordingly
+   *
+   * @memberof Table
+   */
+  setShades() {
+    const scrollPercentage = this.getScrollPercentage();
+    const offsetLeft = 1;
+    const offsetRight = 99;
+
+    if (scrollPercentage > offsetLeft) {
+      this.ui.scrollArea.classList.add(this.options.stateClasses.shadeLeft);
+    } else {
+      this.ui.scrollArea.classList.remove(this.options.stateClasses.shadeLeft);
+    }
+
+    if (scrollPercentage < offsetRight) {
+      this.ui.scrollArea.classList.add(this.options.stateClasses.shadeRight);
+    } else {
+      this.ui.scrollArea.classList.remove(this.options.stateClasses.shadeRight);
+    }
+
+    // Temporarily here, have to find a better solution
+    this.setLeftOffset();
+  }
+
+  getScrollPercentage() {
+    const scrollWrapper = this.ui.scrollAreaWrapper;
+
+    return 100 * scrollWrapper.scrollLeft / (scrollWrapper.scrollWidth - scrollWrapper.clientWidth);
   }
 
   /**
