3cb14b28af6ca830d630e4d867bd0f672c7ee8a3
Marcel Bührig <marcel.buehrig@zeix.com>
2019-06-06 13:00:04 +0200

    Finished content-teaser

---
+++ src/modules/content_nav/content_nav.data.js
@@ -0,0 +1,45 @@
+const _ = require('lodash');
+const dataHelper = require('@unic/estatico-data');
+const { handlebars } = require('@unic/estatico-handlebars');
+const defaultData = require('../../data/default.data.js');
+
+const template = dataHelper.getFileContent('content_nav.hbs');
+const data = _.merge({}, defaultData, {
+  meta: {
+    title: 'ContentNav',
+    className: 'ContentNav',
+    jira: 'CZHDEV-389',
+    documentation: dataHelper.getDocumentation('content_nav.md'),
+  },
+  props: {
+    items: [],
+  },
+});
+const variants = _.mapValues({
+  default: {
+    meta: {
+      title: 'Default',
+      desc: 'Default implementation',
+    },
+  },
+}, (variant) => {
+  const variantProps = _.merge({}, data, variant).props;
+  const compiledVariant = () => handlebars.compile(template)(variantProps);
+  const variantData = _.merge({}, data, variant, {
+    meta: {
+      demo: compiledVariant,
+
+      code: {
+        handlebars: dataHelper.getFormattedHandlebars(template),
+        html: dataHelper.getFormattedHtml(compiledVariant()),
+        data: dataHelper.getFormattedJson(variantProps),
+      },
+    },
+  });
+
+  return variantData;
+});
+
+data.variants = variants;
+
+module.exports = data;
