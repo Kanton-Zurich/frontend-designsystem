981417629bc266f631a1d0295f46fab065608a78
Esther Brunner <esther.brunner@zeix.com>
2019-07-02 18:20:03 +0200

    CZHDEV-505 fix linting errors

--- src/modules/topiclist/topiclist.ts
+++ src/modules/topiclist/topiclist.ts
@@ -5,7 +5,6 @@
  * @copyright
  */
 import Module from '../../assets/js/helpers/module';
-import namespace from '../../assets/js/helpers/namespace';
 
 class Topiclist extends Module {
   public options: {
@@ -25,7 +24,7 @@ class Topiclist extends Module {
       domSelectors: {
         showAllButton: '[data-topiclist="showAllTrigger"]',
         contentNavItems: '[data-init="topiclist"] .mdl-content_nav > ul > li',
-        hiddenContentNavItems: '[data-init="topiclist"] .mdl-content_nav > ul > li.mdl-content_nav__item--hidden'
+        hiddenContentNavItems: '[data-init="topiclist"] .mdl-content_nav > ul > li.mdl-content_nav__item--hidden',
       },
       stateClasses: {
         hiddenItem: 'mdl-content_nav__item--hidden',
@@ -35,7 +34,6 @@ class Topiclist extends Module {
 
     this.initUi();
     this.initEventListeners();
-
   }
 
   static get events() {
@@ -56,7 +54,7 @@ class Topiclist extends Module {
    * Shows all hidden items from the content nav list
    */
   showAll() {
-    (<any>this.ui).hiddenContentNavItems.forEach( element => {
+    (<any> this.ui).hiddenContentNavItems.forEach((element: any) => {
       element.classList.remove(this.options.stateClasses.hiddenItem);
     });
   }
