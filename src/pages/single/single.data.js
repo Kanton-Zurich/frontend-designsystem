24f23ddf7ae4a996ae4d9421287c5dbb31a7919c
hendrikwernze <hendrik.wernze@one-inside.com>
2019-07-03 19:30:58 +0200

    Added button condition to Page Header module; Added modules and data for singlepage;

--- src/pages/single/single.data.js
+++ src/pages/single/single.data.js
@@ -1,10 +1,99 @@
 const _ = require('lodash');
 const defaultData = require('../../data/default.data.js');
-const skiplinksData = require('../../modules/skiplinks/skiplinks.data.js');
-const richtextData = require('../../modules/richtext/richtext.data.js');
-const imageGalleryData = require('../../modules/image_gallery/image_gallery.data.js');
-const videoData = require('../../modules/video/video.data.js');
+const defPageHeaderData = require('../../modules/page_header/page_header.data.js').variants.colored.props;
+const defTopicListData = require('../../modules/topiclist/topiclist.data.js').variants.default.props;
+const defTeaserData = require('../../modules/teaser/teaser.data.js').variants.inverted.props;
+const contentTeaserDataWithoutBuzzwords = require('../../atoms/content_teaser/content_teaser.data').variants.withoutBuzzwords.props;
 const dataHelper = require('@unic/estatico-data');
+
+const defPageHeaderCustomData = {
+  pageTitle: 'biz Kloten',
+  leadText: 'Gerne unterstützen wir Sie bei der Berufs- und Studienwahl sowie bei der Gestaltung Ihrer beruflichen Laufbahn',
+  breadcrumb: {
+    path: [
+      {
+        title: 'Kanton Zürich',
+        href: '#',
+      },
+      {
+        title: '...',
+        href: '#',
+      },
+      {
+        title: 'Amt für Jugend und Berufsberatung',
+        href: '#',
+      },
+      {
+        title: 'Standorte',
+        href: '#',
+      },
+      {
+        title: 'biz Klotzen',
+      },
+    ],
+  },
+};
+
+const defTopicListCustomData = {
+  contentNavData: _.merge({}, {
+    items: [
+      _.merge({}, contentTeaserDataWithoutBuzzwords, {
+        shortTitle: 'Berufsberatuung für Schülerinnen und Schüler',
+      }),
+      _.merge({}, contentTeaserDataWithoutBuzzwords, {
+        shortTitle: 'SOS-Beratung für Jugendliche',
+      }),
+      _.merge({}, contentTeaserDataWithoutBuzzwords, {
+        shortTitle: 'Berufs- und Laufbahnberatung für Erwachsene',
+      }),
+      _.merge({}, contentTeaserDataWithoutBuzzwords, {
+        shortTitle: 'Laufbahnberatung für Erwerbslose',
+      }),
+      _.merge({}, contentTeaserDataWithoutBuzzwords, {
+        shortTitle: 'Laufbahnberatung für Sozialhilfeempfängerinnen und Sozialhilfeempfänger',
+      }),
+    ],
+  }),
+};
+
+const defContactCustomData = {
+  fullWidth: true,
+  contactTitle: 'Kontakt',
+  contactAddress: {
+    name: 'biz Kloten',
+    street: 'Hamelirainstrasse 4',
+    zip: '8302',
+    city: 'Kloten',
+    routeLinkHref: '#',
+    routeLinkLabel: 'Route anzeigen',
+    openingTimes: [{
+      timeTitle: 'Bürozeiten',
+      times: [
+        { text: 'Mo-Fr: 8.00 - 11:30 &' },
+        { text: '13:30 - 17:00' },
+      ],
+    },
+    {
+      timeTitle: 'Öffnungszeiten Infothek',
+      times: [
+        { text: 'Mo./Di./Mi./Fr.: 13.30 - 17:30 &' },
+        { text: 'Do.: 13:30 - 19:00' },
+      ],
+    }],
+  },
+  contactPhone: [
+    {
+      anchorLabel: '043 259 82 00',
+      phoneNumer: '+41432598200',
+      additionalInfo: 'Telefon',
+    },
+  ],
+};
+
+const defTeaserCustomData = {
+  teaserTitle: 'Werden Sie Teil unseres Teams',
+  teaserDescription: 'Wir bieten Ihnen ein spannendes Tätigkeitsfeld und eine angenehme Arbeitsumgebung',
+}
 
 const data = _.merge({}, defaultData, {
   meta: {
@@ -17,11 +106,10 @@ const data = _.merge({}, defaultData, {
     title: 'Einzelseite',
     text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
     modules: {
-      // xy: require('../../modules/xy/xy.data.js').props
-      skiplinks: skiplinksData.props,
-      richtext: richtextData.props,
-      imageGallery: imageGalleryData.props,
-      video: videoData.props,
+      pageHeaderData: _.merge({}, _.omit(defPageHeaderData, ['buttonData']), defPageHeaderCustomData),
+      topicListData: _.merge({}, _.omit(defTopicListData, ['contentNavData']), defTopicListCustomData),
+      contactData: defContactCustomData,
+      teaserData: _.merge({}, defTeaserData, defTeaserCustomData),
     },
   },
 });
