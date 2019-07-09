fe38a29574b6a3af9a055aab2561c0d5fc930dcb
Robert Wüest <robert.wueest@one-inside.com>
2019-07-09 09:33:34 +0200

    CZHDEV-741 Extract paragraph from richtext - Bugfix line clamp flashing (reveal on line clamp) - Fix margins
    Bugs resolved:
    -CZHDEV-682
    -CZHDEV-724
    -CZHDEV-681
    
    !IMPORTANT!: Markup changed for following elements:
    -Anleitung(instructions)
    -Verwandet Inhalte (related_content)
    -Text (richtext)
    -Schwerpunkt Teaser (focus_teaser)
    -Themenliste (topilist)
    -Schwerpunktseite (focus page)
    -Amtsseite (administration page)

---
+++ src/atoms/paragraph/paragraph.data.js
@@ -0,0 +1,55 @@
+const _ = require('lodash');
+const dataHelper = require('@unic/estatico-data');
+const { handlebars } = require('@unic/estatico-handlebars');
+const defaultData = require('../../data/default.data.js');
+
+const template = dataHelper.getFileContent('paragraph.hbs');
+const data = _.merge({}, defaultData, {
+  meta: {
+    title: 'Paragraph',
+    className: 'atm-paragraph',
+    jira: 'CZHDEV-741',
+    documentation: dataHelper.getDocumentation('paragraph.md'),
+    hideFromListing: false,
+  },
+  props: {
+    paragraphText: 'P, Regular Interessierte können ab sofort die Genauigkeit ihrer Smartphones und Navigationsgeräte überprüfen. Die'
+      + ' Baudirektion hat beim <a href="#" class="atm-text_link">Landesmuseum</a> in Zürich einen Kontrollpunkt beim Landesmuseum in Zürich einen Kontrollpunktfür'
+      + ' mobile Geräte eingerichtet – den ersten in der Schweiz.P, Helvetic Roman Interessierte können ab sofort die'
+      + ' Genauigkeit ihrer Smartphones und Navigationsgeräte überprüfen.'
+      + ' Die Baudirektion hat beim Landesmuseum in Zürich einen Kontrollpunkt beim Landesmuseum in Zürich einen'
+      + ' Kontrollpunktfür mobile Geräte eingerichtet – den ersten in der Schweiz.',
+  },
+});
+
+data.colorVariations = []; // no color variations available
+
+const variants = _.mapValues({
+  default: {
+    meta: {
+      title: 'default',
+    },
+    props: {
+    },
+  },
+}, (variant) => {
+  const variantProps = _.merge({}, data, variant).props;
+  const compiledVariant = () => handlebars.compile(template)(variantProps);
+  const variantData = _.merge({}, data, variant, {
+    meta: {
+      demo: compiledVariant,
+
+      code: {
+        handlebars: dataHelper.getFormattedHandlebars(template),
+        html: dataHelper.getFormattedHtml(compiledVariant()),
+        data: dataHelper.getFormattedJson(variantProps),
+      },
+    },
+  });
+
+  return variantData;
+});
+
+data.variants = variants;
+
+module.exports = data;
