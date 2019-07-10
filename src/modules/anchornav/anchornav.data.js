3df9144c550d555a691e9d8fe0ce8843d19b0a55
hendrikwernze <hendrik.wernze@one-inside.com>
2019-07-10 12:57:07 +0200

    Initial commit created module base and added a bit custom data;

---
+++ src/modules/anchornav/anchornav.data.js
@@ -0,0 +1,120 @@
+const _ = require('lodash');
+const dataHelper = require('@unic/estatico-data');
+const { handlebars } = require('@unic/estatico-handlebars');
+const defaultData = require('../../data/default.data.js');
+
+const template = dataHelper.getFileContent('anchornav.hbs');
+const data = _.merge({}, defaultData, {
+  meta: {
+    title: 'Anchornav',
+    className: 'Anchornav',
+    jira: 'CZHDEV-*',
+    documentation: dataHelper.getDocumentation('anchornav.md'),
+  },
+  props: {
+    anchornavTitle: {
+      level: 5,
+      title: 'Inhaltsverzeichniss',
+    },
+    anchornavItems: [
+      {
+        anchorlink: {
+          anchorlinkText: 'Themen',
+          anchorlinkAdress: '#themen',
+          anchorlinkIsActive: true,
+        },
+      },
+      {
+        anchorlink: {
+          anchorlinkText: 'Service',
+          anchorlinkAdress: '#service',
+          anchorlinkIsActive: false,
+        },
+      },
+      {
+        anchorlink: {
+          anchorlinkText: 'Unterlagen',
+          anchorlinkAdress: '#unterlagen',
+          anchorlinkIsActive: false,
+        },
+      },
+      {
+        anchorlink: {
+          anchorlinkText: 'Weiterführende Informationen',
+          anchorlinkAdress: '#infos',
+          anchorlinkIsActive: false,
+        },
+      },
+      {
+        anchorlink: {
+          anchorlinkText: 'Kontakt',
+          anchorlinkAdress: '#contact',
+          anchorlinkIsActive: false,
+        },
+      },
+      {
+        anchorlink: {
+          anchorlinkText: 'Standorte',
+          anchorlinkAdress: '#locations',
+          anchorlinkIsActive: false,
+        },
+      },
+      {
+        anchorlink: {
+          anchorlinkText: 'Bildergallerie',
+          anchorlinkAdress: 'https://www.google.ch/?hl=de',
+          anchorlinkIsActive: false,
+          anchorlinkIsOpensInNewTab: true,
+        },
+      },
+      {
+        anchorlink: {
+          anchorlinkText: 'Download',
+          anchorlinkAdress: 'https://developer.mozilla.org/en-US/',
+          anchorlinkIsActive: false,
+          anchorlinkIsOpensInNewTab: true,
+        },
+      },
+    ],
+  },
+});
+const variants = _.mapValues({
+  default: {
+    meta: {
+      title: 'Default',
+      desc: 'Default implementation',
+    },
+    props: {
+      isSticky: false,
+    },
+  },
+  sticky: {
+    meta: {
+      title: 'Sticky',
+      desc: 'Navigation bleibt immer am oberen Rand kleben.',
+    },
+    props: {
+      isSticky: true,
+    },
+  },
+}, (variant) => {
+  const variantProps = _.merge({}, data, variant).props;
+  const compiledVariant = () => handlebars.compile(template)(variantProps);
+  const variantData = _.merge({}, data, variant, {
+    meta: {
+      demo: compiledVariant,
+
+      code: {
+        handlebars: dataHelper.getFormattedHandlebars(template),
+        html: dataHelper.getFormattedHtml(compiledVariant()),
+        data: dataHelper.getFormattedJson(variantProps),
+      },
+    },
+  });
+
+  return variantData;
+});
+
+data.variants = variants;
+
+module.exports = data;
