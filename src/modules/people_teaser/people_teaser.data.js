de6d17a7ad663fe2a5008a00aa4bf3d6c6a7e324
Robert Wüest <robert.wueest@one-inside.com>
2019-07-12 10:27:07 +0200

    Add initial modules

---
+++ src/modules/people_teaser/people_teaser.data.js
@@ -0,0 +1,45 @@
+const _ = require('lodash');
+const dataHelper = require('@unic/estatico-data');
+const { handlebars } = require('@unic/estatico-handlebars');
+const defaultData = require('../../data/default.data.js');
+
+const template = dataHelper.getFileContent('people_teaser.hbs');
+const data = _.merge({}, defaultData, {
+  meta: {
+    title: 'people_teaser',
+    className: 'PeopleTeaser',
+    jira: 'CZHDEV-*',
+    documentation: dataHelper.getDocumentation('people_teaser.md'),
+  },
+  props: {
+
+  },
+});
+const variants = _.mapValues({
+  default: {
+    meta: {
+      title: 'Default',
+      desc: 'Default implementation',
+    },
+  },
+}, (variant) => {
+  const variantProps = _.merge({}, data, variant).props;
+  const compiledVariant = () => handlebars.compile(template)(variantProps);
+  const variantData = _.merge({}, data, variant, {
+    meta: {
+      demo: compiledVariant,
+
+      code: {
+        handlebars: dataHelper.getFormattedHandlebars(template),
+        html: dataHelper.getFormattedHtml(compiledVariant()),
+        data: dataHelper.getFormattedJson(variantProps),
+      },
+    },
+  });
+
+  return variantData;
+});
+
+data.variants = variants;
+
+module.exports = data;
---
+++ src/modules/person_card/person_card.data.js
@@ -0,0 +1,45 @@
+const _ = require('lodash');
+const dataHelper = require('@unic/estatico-data');
+const { handlebars } = require('@unic/estatico-handlebars');
+const defaultData = require('../../data/default.data.js');
+
+const template = dataHelper.getFileContent('person_card.hbs');
+const data = _.merge({}, defaultData, {
+  meta: {
+    title: 'person-card',
+    className: 'PersonCard',
+    jira: 'CZHDEV-*',
+    documentation: dataHelper.getDocumentation('person_card.md'),
+  },
+  props: {
+
+  },
+});
+const variants = _.mapValues({
+  default: {
+    meta: {
+      title: 'Default',
+      desc: 'Default implementation',
+    },
+  },
+}, (variant) => {
+  const variantProps = _.merge({}, data, variant).props;
+  const compiledVariant = () => handlebars.compile(template)(variantProps);
+  const variantData = _.merge({}, data, variant, {
+    meta: {
+      demo: compiledVariant,
+
+      code: {
+        handlebars: dataHelper.getFormattedHandlebars(template),
+        html: dataHelper.getFormattedHtml(compiledVariant()),
+        data: dataHelper.getFormattedJson(variantProps),
+      },
+    },
+  });
+
+  return variantData;
+});
+
+data.variants = variants;
+
+module.exports = data;
