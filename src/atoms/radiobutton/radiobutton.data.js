26bbb0eb399a2eff6ef33fb4f1a7fbd2ee5c9cd2
hendrikwernze <hendrik.wernze@one-inside.com>
2019-08-05 16:16:20 +0200

    Added radio-button atom;

---
+++ src/atoms/radiobutton/radiobutton.data.js
@@ -0,0 +1,63 @@
+const _ = require('lodash');
+const dataHelper = require('@unic/estatico-data');
+const { handlebars } = require('@unic/estatico-handlebars');
+const defaultData = require('../../data/default.data.js');
+
+const template = dataHelper.getFileContent('radiobutton.hbs');
+const data = _.merge({}, defaultData, {
+  meta: {
+    title: 'Radiobutton',
+    className: 'Radiobutton',
+    jira: 'CZHDEV-847',
+    documentation: dataHelper.getDocumentation('radiobutton.md'),
+  },
+  props: {
+
+  },
+});
+const variants = _.mapValues({
+  default: {
+    meta: {
+      title: 'Default',
+      desc: 'Default implementation',
+    },
+  },
+  checked: {
+    meta: {
+      title: 'Vorausgewählt',
+      desc: 'Initial Ausgewählt/aktiv.',
+    },
+    props: {
+      isChecked: true,
+    },
+  },
+  disabled: {
+    meta: {
+      title: 'Deaktiviert',
+      desc: 'Initial deaktiviert, nicht click- bzw auswählbar.',
+    },
+    props: {
+      isDisabled: true,
+    },
+  },
+}, (variant) => {
+  const variantProps = _.merge({}, data, variant).props;
+  const compiledVariant = () => handlebars.compile(template)(variantProps);
+  const variantData = _.merge({}, data, variant, {
+    meta: {
+      demo: compiledVariant,
+
+      code: {
+        handlebars: dataHelper.getFormattedHandlebars(template),
+        data: dataHelper.getFormattedJson(variantProps),
+        html: dataHelper.getFormattedHtml(compiledVariant()),
+      },
+    },
+  });
+
+  return variantData;
+});
+
+data.variants = variants;
+
+module.exports = data;
