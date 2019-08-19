b9c5ff085cb9960757abb34a71987fd49fdf367d
Marcel Bührig <marcel.buehrig@zeix.com>
2019-08-19 11:13:52 +0200

    Fix of templateConverter

--- gulp/helpers/templateConverter.js
+++ gulp/helpers/templateConverter.js
@@ -1,14 +1,14 @@
 module.exports = (handlebarsTemplate, purgeConditionals = true) => {
   let output = handlebarsTemplate;
   if (purgeConditionals) {
-    output = output.replace(/{{#if (.*?)}}/gm, '');
-    output = output.replace(/{{\/if}}/gm, '');
+    output = output.replace(/{{#if (.*?)\/if}}/gm, '');
   } else {
     output = output.replace(/{{#if (.*?)}}/gm, (m) => {
       const attr = m.replace(' ', '').replace(/{{#if/gm, '').replace('}}', '');
       return `<% if (typeof(${attr}) !== "undefined" && ${attr} !== false) { %>`;
     }).replace(/{{\/if}}/gm, '<% } %>');
   }
-  return output.replace(/{{{/gm, '${').replace(/}}}/gm, '}')
-    .replace(/{{/gm, '${').replace(/}}/gm, '}');
+
+  return output.replace(/{{{/gm, '<%=').replace(/}}}/gm, '%>')
+    .replace(/{{/gm, '<%=').replace(/}}/gm, '%>');
 };
