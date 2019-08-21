3f93f388d6fc24e02194bb588d3b2b439f6e4f8c
Marcel Bührig <marcel.buehrig@zeix.com>
2019-08-21 09:41:17 +0200

    Finished duplicator

--- src/assets/js/helpers/duplication.class.ts
+++ src/assets/js/helpers/duplication.class.ts
@@ -1,4 +1,5 @@
 import { uniqueId } from 'lodash';
+import { Delegate } from 'dom-delegate';
 
 class DuplicationElement {
   public ui: {
@@ -7,6 +8,12 @@ class DuplicationElement {
     button: HTMLButtonElement,
   }
 
+  public options: {
+    eventSelectors: any,
+  }
+
+  private eventDelegate: any;
+
   constructor(element) {
     this.ui = {
       element,
@@ -14,7 +21,16 @@ class DuplicationElement {
       template: element.querySelector('script'),
     };
 
-    this.initEventListeners(this.ui.element);
+    this.options = {
+      eventSelectors: {
+        duplicator: '[data-duplicate="duplicator"]',
+        remover: '[data-duplicate="remover"]',
+      },
+    };
+
+    this.eventDelegate = new Delegate(element);
+
+    this.initEventListeners();
   }
 
   static get events() {
@@ -23,14 +39,17 @@ class DuplicationElement {
     };
   }
 
-  initEventListeners(targetElement) {
-    targetElement.querySelector('.form__duplicator').addEventListener('click', this.duplicateItself.bind(this));
+  initEventListeners() {
+    this.eventDelegate.on('click', this.options.eventSelectors.duplicator, this.duplicateItself.bind(this));
+    this.eventDelegate.on('click', this.options.eventSelectors.remover, this.removeDuplication.bind(this));
   }
 
   duplicateItself() {
     const parsedHTML = new DOMParser().parseFromString(this.ui.template.innerHTML, 'text/html').querySelector('div');
     const uid = uniqueId();
-    const fields = parsedHTML.querySelectorAll('[for], [name], [id]');
+    const fields = parsedHTML.querySelectorAll('[for], [name], [id], [data-remove-uid]');
+
+    parsedHTML.setAttribute('data-uid', uid);
 
     fields.forEach((element) => {
       if (element.hasAttribute('name')) {
@@ -42,14 +61,23 @@ class DuplicationElement {
       if (element.hasAttribute('for')) {
         element.setAttribute('for', `${element.getAttribute('for')}_${uid}`);
       }
+      if (element.hasAttribute('data-remove-uid')) {
+        element.setAttribute('data-remove-uid', uid);
+      }
     });
 
     this.ui.element.appendChild(parsedHTML);
-    this.initEventListeners(parsedHTML);
 
     this.ui.element.dispatchEvent(new CustomEvent(DuplicationElement.events.domReParsed, {
       detail: parsedHTML,
     }));
+  }
+
+  removeDuplication(event, delegate) {
+    const button = delegate;
+    const duplication = this.ui.element.querySelector(`[data-uid="${button.getAttribute('data-remove-uid')}"]`);
+
+    duplication.parentNode.removeChild(duplication);
   }
 }
 
