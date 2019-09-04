571fb5d1d4cc51d730cdac395fdbd65a4ebc6bee
Esther Brunner <esther.brunner@zeix.com>
2019-09-04 17:35:56 +0200

    CZHDEV-176 live stream video

--- src/modules/live_stream/live_stream.data.js
+++ src/modules/live_stream/live_stream.data.js
@@ -12,7 +12,11 @@ const data = _.merge({}, defaultData, {
     documentation: dataHelper.getDocumentation('live_stream.md'),
   },
   props: {
-
+    liveStreamHeading: {
+      level: 2,
+      title: 'Live Stream Video',
+    },
+    videoUrl: 'https://www.blitzvideoserver.de/player.html?serverip=62.113.210.2&serverapp=vod&smil=video-stream-hosting-testvideo.smil&stream360p=video-stream-hosting-testvideo_360p.mp4&stream480p=video-stream-hosting-testvideo_480p.mp4&stream720p=video-stream-hosting-testvideo_720p.mp4&stream1080p=video-stream-hosting-testvideo_1080p.mp4&bgimage=https://www.blitzvideoserver.de/video-stream-hosting-testvideo.jpg&livestreamnichtaktiv=funktioniert%20nicht&livestreamnichtaktivuntertitel=Abruf%20nicht%20m%C3%B6glich%0A&dateistreamnichtaktiv=gehtnicht&dateistreamnichtaktivuntertitel=datei%20nicht%20vorhanden',
   },
 });
 const variants = _.mapValues({
