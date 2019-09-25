22ee3cd13474fa24dd4923892bdd2b37a740b047
Stefan Grell <sg@folge3.de>
2019-09-25 13:45:43 +0200

    Added custom map controls Implemented user locate

--- src/modules/map_view/map_view.data.js
+++ src/modules/map_view/map_view.data.js
@@ -2,6 +2,7 @@ const _ = require('lodash');
 const dataHelper = require('@unic/estatico-data');
 const { handlebars } = require('@unic/estatico-handlebars');
 const defaultData = require('../../data/default.data.js');
+const defButtonData = require('../../atoms/button/button.data').variants.default.props;
 
 const template = dataHelper.getFileContent('map_view.hbs');
 const data = _.merge({}, defaultData, {
@@ -12,7 +13,25 @@ const data = _.merge({}, defaultData, {
     documentation: dataHelper.getDocumentation('map_view.md'),
   },
   props: {
-
+    withUserLocate: true,
+    centerBtn: _.merge({}, defButtonData, {
+      isTextVisible: false,
+      icon: 'locate',
+      isInverted: true,
+      additionalAttribute: 'date-map-view="centerBtn"',
+    }),
+    zoomInBtn: _.merge({}, defButtonData, {
+      isTextVisible: false,
+      icon: 'plus',
+      isInverted: true,
+      additionalAttribute: 'date-map-view="zoomInBtn"',
+    }),
+    zoomOutBtn: _.merge({}, defButtonData, {
+      isTextVisible: false,
+      icon: 'minus',
+      isInverted: true,
+      additionalAttribute: 'date-map-view="zoomOutBtn"',
+    }),
   },
 });
 const variants = _.mapValues({
--- src/modules/map_view/map_view.ts
+++ src/modules/map_view/map_view.ts
@@ -9,26 +9,53 @@ import * as L from 'leaflet';
 import * as wms from 'leaflet.wms';
 
 /* eslint-disable no-magic-numbers */
-const mapOptions = {
+const mapOptions: L.MapOptions = {
   crs: L.CRS.EPSG3857,
   maxBounds: L.latLngBounds(L.latLng(48, 5.8), L.latLng(45.6, 10.9)),
   maxBoundsViscosity: 0.5,
   center: L.latLng(47.3776662, 8.5365413),
   zoom: 12,
   maxZoom: 18,
-  minZoom: 9,
+  minZoom: 11,
   zoomAnimation: false,
+  zoomControl: false,
+  attributionControl: false,
 };
 /* eslint-enable no-magic-numbers */
-const kzhIcon = L.divIcon({ className: 'mdl-map__marker', iconSize: [0, 0] });
+const kzhIcon = L.divIcon({ className: 'mdl-map_view__marker', iconSize: [0, 0] });
+const userPosIcon = L.divIcon({ className: 'mdl-map_view__userposition', iconSize: [0, 0] });
 
 class MapView extends Module {
+  public options: {
+    domSelectors: {
+      zoomInBtn: string,
+      zoomOutBtn: string,
+      centerBtn: string,
+    }
+    stateClasses: {
+
+    }
+  };
+
+  public ui: {
+    element: Element,
+    zoomInBtn: HTMLElement,
+    zoomOutBtn: HTMLElement,
+    centerBtn: HTMLElement,
+  };
+
+  private map: L.Map;
+
+  private userPosMarker: L.Marker;
+
   constructor($element: any, data: Object, options: Object) {
     const defaultData = {
     };
     const defaultOptions = {
       domSelectors: {
-        // item: '[data-${{{className}}.name}="item"]'
+        zoomInBtn: '[date-map-view=zoomInBtn]',
+        zoomOutBtn: '[date-map-view=zoomOutBtn]',
+        centerBtn: '[date-map-view=centerBtn]',
       },
       stateClasses: {
         // activated: 'is-activated'
@@ -54,12 +81,25 @@ class MapView extends Module {
    */
   initEventListeners() {
     // Event listeners
+    this.eventDelegate
+      .on('click', this.options.domSelectors.zoomInBtn, () => {
+        this.map.zoomIn();
+      })
+      .on('click', this.options.domSelectors.zoomOutBtn, () => {
+        this.map.zoomOut();
+      })
+      .on('click', this.options.domSelectors.centerBtn, () => {
+        this.map.locate();
+      });
   }
 
   private initMap(): void {
     const url = 'https://wms.zh.ch/ZHWEB?';
     const map = new L.Map('map', mapOptions);
 
+    // L.control.zoom({ position: 'bottomright' }).addTo(map);
+    // L.control.locate().addTo(map);
+    // map.locate()
     L.tileLayer.wms(url, {
       version: '1.3.0',
       format: 'image/png; mode=8bit',
@@ -74,15 +114,43 @@ class MapView extends Module {
       layers: 'ZHLabels',
     }).addTo(map);
 
-    L.marker([47.3776662,8.5365413], { icon: kzhIcon }).addTo(map);
+    L.marker(
+      [47.3776662, 8.5365413],
+      { icon: kzhIcon },
+    ).addTo(map);
+
+    if (this.ui.centerBtn) {
+      this.log('User locate enabled. Requesting user location.');
+      map.locate();
+      map.on('locationfound', (ev: L.LocationEvent) => {
+        this.log('Locationfound event: ', ev);
+        const userLatLng = ev.latlng;
+        if (userLatLng) {
+          if (!this.userPosMarker) {
+            this.userPosMarker = L.marker(
+              [47.4341, 8.46874], // userLatLng, TODO: For dev only
+              { icon: userPosIcon },
+            ).addTo(map);
+          } else {
+            this.userPosMarker.setLatLng(userLatLng);
+          }
+        }
+      });
+      map.on('locationerror', (errorEv: L.ErrorEvent) => {
+        this.log('Failed to locate user.');
+        this.log('Locationerror event: ', errorEv);
+      });
+    }
 
     // Zur Überprüfung, ob alle aktuellen Layer angezeigt werden
-    // L.WMS.layer(url, 'ZHWEB', {
-    //   version: '1.3.0',
-    //   format: 'image/png; mode=8bit',
-    //   transparent: true,
-    //   layers: 'ZHWEB'
-    // }).addTo(map);
+    wms.layer(url, 'ZHWEB', {
+      version: '1.3.0',
+      format: 'image/png; mode=8bit',
+      transparent: true,
+      layers: 'ZHWEB',
+    }).addTo(map);
+
+    this.map = map;
   }
 
   /**
