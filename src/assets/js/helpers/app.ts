edcb4800aba02fdd2a77face1c75199e3b22e9d0
hendrikwernze <hendrik.wernze@one-inside.com>
2019-10-09 15:07:32 +0200

    initial commit;

--- src/assets/js/helpers/app.ts
+++ src/assets/js/helpers/app.ts
@@ -38,6 +38,8 @@ import NewsFilterMobile from '../../../modules/news_filter_mobile/news_filter_mo
 import NewsOverview from '../../../modules/news_overview/news_overview';
 import Locations from '../../../modules/locations/locations';
 import MapView from '../../../modules/map_view/map_view';
+import UserMenu from '../../../modules/user_menu/user_menu';
+import LoginForm from '../../../modules/login_form/login_form';
 /* autoinsertmodulereference */ // eslint-disable-line
 
 import Form from './form.class';
@@ -85,6 +87,8 @@ class App {
     this.modules.newsOverview = NewsOverview;
     this.modules.locations = Locations;
     this.modules.mapView = MapView;
+    this.modules.userMenu = UserMenu;
+    this.modules.loginForm = LoginForm;
     /* autoinsertmodule */ // eslint-disable-line
 
     // expose initModule function
---
+++ src/modules/user_menu/user_menu.data.js
@@ -0,0 +1,45 @@
+const _ = require('lodash');
+const dataHelper = require('@unic/estatico-data');
+const { handlebars } = require('@unic/estatico-handlebars');
+const defaultData = require('../../data/default.data.js');
+
+const template = dataHelper.getFileContent('user_menu.hbs');
+const data = _.merge({}, defaultData, {
+  meta: {
+    title: 'User Menu',
+    className: 'UserMenu',
+    jira: 'CZHDEV-*',
+    documentation: dataHelper.getDocumentation('user_menu.md'),
+  },
+  props: {
+
+  },
+});
+const variants = _.mapValues({
+  default: {
+    meta: {
+      title: 'Default',
+      desc: 'Default implementation',
+    },
+  },
+}, (variant) => {
+  const variantProps = _.merge({}, data, variant).props;
+  const compiledVariant = () => handlebars.compile(template)(variantProps);
+  const variantData = _.merge({}, data, variant, {
+    meta: {
+      demo: compiledVariant,
+
+      code: {
+        handlebars: dataHelper.getFormattedHandlebars(template),
+        html: dataHelper.getFormattedHtml(compiledVariant()),
+        data: dataHelper.getFormattedJson(variantProps),
+      },
+    },
+  });
+
+  return variantData;
+});
+
+data.variants = variants;
+
+module.exports = data;
