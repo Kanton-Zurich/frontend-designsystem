56757e18c95be8a36bc9b25642db8c844a0ec44b
Esther Brunner <esther.brunner@zeix.com>
2019-10-15 16:09:43 +0200

    CZHDEV-1240 WIP ZHLex

--- src/atoms/list/list.data.js
+++ src/atoms/list/list.data.js
@@ -143,6 +143,34 @@ const variants = _.mapValues({
       isSingleSelect: true,
     },
   },
+  table: {
+    meta: {
+      title: 'Auswahltabelle',
+      desc: 'Einzelauswahl mit tabellarischer Darstellung der Auswahlliste',
+    },
+    props: {
+      groupId: 'tableSingle',
+      isSingleSelect: true,
+      isTable: true,
+      selectTableHeaders: ['Nr.', 'Ordnungsnummer', 'Thema'],
+      selectOptions: [
+        { value: '1', label: '1', selectTableCells: ['101 - 176', 'Verfassung - Kantonsgebiet - Gemeinden - Bürgerrecht - Politische Rechte - Behörden'], id: _.uniqueId('option-item') },
+        { value: '2', label: '2', selectTableCells: ['177 - 184', 'Staatspersonal - Kirchen - Religionsgemeinschaften'], id: _.uniqueId('option-item') },
+        { value: '3', label: '3', selectTableCells: ['211 - 255', 'Gerichtsorganisation - Zivilrecht - Notariat - Grundbuch'], id: _.uniqueId('option-item') },
+        { value: '4', label: '4', selectTableCells: ['281 - 351', 'Schuldbetreibung und Konkurs - Strafrecht - Strafvollzug - Opferhilfe - Gewaltschutz'], id: _.uniqueId('option-item') },
+        { value: '5', label: '5', selectTableCells: ['410 - 412', 'Bildung - Volksschule'], id: _.uniqueId('option-item') },
+        { value: '6', label: '6', selectTableCells: ['413', 'Mittelschulen - Berufsbildung'], id: _.uniqueId('option-item') },
+        { value: '7', label: '7', selectTableCells: ['414', 'Fachhochschulen'], id: _.uniqueId('option-item') },
+        { value: '8', label: '8', selectTableCells: ['415 - 440', 'Universität - Dokumentation - Kultur'], id: _.uniqueId('option-item') },
+        { value: '9', label: '9', selectTableCells: ['511 - 554', 'Militär - Bevölkerungsschutz - Polizei'], id: _.uniqueId('option-item') },
+        { value: '10', label: '10', selectTableCells: ['611 - 691', 'Finanzhaushalt - Steuern - Gebühren'], id: _.uniqueId('option-item') },
+        { value: '11', label: '11', selectTableCells: ['700 - 715', 'Raumplanung - Baurecht - Umweltschutz'], id: _.uniqueId('option-item') },
+        { value: '12', label: '12', selectTableCells: ['720 - 782', 'Beschaffungswesen - Strassen - Wasserwirtschaft - Energie - Verkehr - Enteignung'], id: _.uniqueId('option-item') },
+        { value: '13', label: '13', selectTableCells: ['810 - 857', 'Gesundheit - Arbeit - Sozialversicherung - Fürsorge'], id: _.uniqueId('option-item') },
+        { value: '14', label: '14', selectTableCells: ['861 - 954', 'Feuerpolizei - Landwirtschaft - Wald und Jagd - Industrie und Gewerbe - Handel - Banken'], id: _.uniqueId('option-item') },
+      ],
+    },
+  },
 }, (variant) => {
   const variantProps = _.merge({}, data, variant).props;
   const compiledVariant = () => handlebars.compile(template)(variantProps);
