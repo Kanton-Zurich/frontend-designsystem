083136b5c5460ebe97ff73f75e1724e5c325c51c
hendrikwernze <hendrik.wernze@one-inside.com>
2019-11-19 13:47:26 +0100

    Added --runInBand parameter to jest start parameters;

--- gulpfile.js
+++ gulpfile.js
@@ -470,7 +470,7 @@ gulp.task('js:test', (done) => { // eslint-disable-line consistent-return
   let killed = false;
   let teardownFailed = false;
 
-  const tests = spawn('npm', ['run', 'jest'].concat(env.ci ? ['--', '--ci', '--i'] : []), {
+  const tests = spawn('npm', ['run', 'jest'].concat(env.ci ? ['--', '--ci', '--i', '--runInBand'] : []), {
     // Add proper output coloring unless in CI env (where this would have weird side-effects)
     stdio: env.ci ? 'pipe' : ['inherit', 'inherit', 'pipe'],
   });
