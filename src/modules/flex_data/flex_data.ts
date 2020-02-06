bc0c10bf087e7849170e08645201180ec5e39d4b
Esther Brunner <esther.brunner@zeix.com>
2020-02-06 10:23:56 +0100

    CZHDEV-1961 updateSortDropdown() method extracted

--- src/modules/flex_data/flex_data.ts
+++ src/modules/flex_data/flex_data.ts
@@ -140,15 +140,8 @@ class FlexData extends Module {
         button.addEventListener('click', () => {
           this.order = button.getAttribute('data-sort-direction');
           this.orderBy = button.getAttribute('data-sort-column');
-          this.ui.genericSortDropdown.classList.remove('visible');
-          this.ui.genericSortDropdown.querySelectorAll('button').forEach((otherButton) => {
-            otherButton.classList.remove('atm-context_menu_item--selected');
-            otherButton.setAttribute('aria-pressed', 'false');
-          });
-          button.classList.add('atm-context_menu_item--selected');
-          button.setAttribute('aria-pressed', 'true');
-          this.ui.genericSortButton.querySelector<HTMLSpanElement>('.atm-form_input__trigger-value').innerText = button.querySelector('span').innerText;
-          this.ui.genericSortButton.setAttribute('aria-expanded', 'false');
+          this.updateSortDropdown(button);
+          this.closeSortDropdown();
           this.ui.paginationInput.value = '1';
           this.loadResults();
         });
@@ -211,13 +204,33 @@ class FlexData extends Module {
     this.loadResults();
   }
 
+  /**
+   * Close sort dropdown
+   */
   private closeSortDropdown() {
     this.ui.genericSortDropdown.classList.remove('visible');
     this.ui.genericSortButton.setAttribute('aria-expanded', 'false');
   }
 
   /**
+   * Update sort dropdown
+   *
+   * @param sortSetting Element
+   */
+  private updateSortDropdown(sortSetting: Element) {
+    this.ui.genericSortDropdown.querySelectorAll('button').forEach((button) => {
+      button.classList.remove('atm-context_menu_item--selected');
+      button.setAttribute('aria-pressed', 'false');
+    });
+    sortSetting.classList.add('atm-context_menu_item--selected');
+    sortSetting.setAttribute('aria-pressed', 'true');
+    this.ui.genericSortButton.querySelector<HTMLSpanElement>('.atm-form_input__trigger-value').innerText = sortSetting.querySelector('span').innerText;
+  }
+
+  /**
    * Load results
+   *
+   * @param scroll boolean
    */
   private loadResults(scroll = false) {
     this.paginationInteraction = false;
@@ -467,13 +480,7 @@ class FlexData extends Module {
           sortSelector += `[data-sort-direction="${this.order}"]`;
         }
         const sortSetting = this.ui.genericSortDropdown.querySelector(sortSelector);
-        this.ui.genericSortDropdown.querySelectorAll('button').forEach((otherButton) => {
-          otherButton.classList.remove('atm-context_menu_item--selected');
-          otherButton.setAttribute('aria-pressed', 'false');
-        });
-        sortSetting.classList.add('atm-context_menu_item--selected');
-        sortSetting.setAttribute('aria-pressed', 'true');
-        this.ui.genericSortButton.querySelector<HTMLSpanElement>('.atm-form_input__trigger-value').innerText = sortSetting.querySelector('span').innerText;
+        this.updateSortDropdown(sortSetting);
       }
     });
   }
