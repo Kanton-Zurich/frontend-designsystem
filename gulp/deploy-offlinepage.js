eb0f7b2778fe6cbdfe308cf96b0653fcc395ab6d
Robert Wüest <robert.wueest@one-inside.com>
2020-02-14 16:24:51 +0100

    FLat offline assets and merge

--- gulp/deploy-offlinepage.js
+++ gulp/deploy-offlinepage.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
-const gulpUtil = require('gulp-util');
+const fs = require('fs');
+const path = require('path');
 const gulpRename = require('gulp-rename');
 const strReplace = require('gulp-string-replace');
 const through = require('through2');
@@ -8,13 +9,33 @@ const task = {
   name: 'deploy:offlinepage',
 };
 
+const mimeTypes = {
+  png: 'data:image/png;base64,',
+  jpg: 'data:image/jpg;base64,',
+  jpeg: 'data:image/jpeg;base64,',
+  gif: 'data:image/gif;base64,',
+  ico: 'data:image/ico;base64,',
+};
+
+const staticBasePath = './dist';
+
+const loadFileFromLocationSync = (url, options = null) => {
+  const resolvedBase = path.resolve(staticBasePath);
+  const safeSuffix = path.normalize(url).replace(/^(\.\.[\/\\])+/, '');
+  const fileLoc = path.join(resolvedBase, safeSuffix);
+
+  return fs.readFileSync(fileLoc, options);
+};
+
+
 gulp.task(task.name, () => {
-  const regxSVG = /\.\.\/\.\.\/assets\/media\/svgsprite\/base.*?\.svg/g;
-  const regxMainMin = /\.\.\/\.\.\/assets\/css\/main.*?\.min\.css/g;
-  const regxPrintMin = /\.\.\/\.\.\/assets\/css\/print.*?\.min\.css/g;
-  const regxFontsMin = /\.\.\/\.\.\/assets\/css\/fonts.*?\.min\.css/g;
-  const regxMainJSMin = /\.\.\/\.\.\/assets\/js\/main.*?\.min\.js/g;
-  const regxHeadJSMin = /\.\.\/\.\.\/assets\/js\/head.*?\.min\.js/g;
+  const linkRegex = /(href|src)="[^\s]+assets[^\s]+(png|jpg|jpeg|ico)"/g;
+  const manifestRegex = /rel="manifest".+(href|src)="[^\s]+assets[^\s]+json"/g;
+  const jsRegex = /<script.+src="[^\s]+assets[^\s]+js".+<\/script>/g;
+  const cssRegex = /<link.+href="[^\s]+assets[^\s]+css".*>/g;
+  const svgRegex = /<!--svg href="[^\s]+"-->/g;
+  const imgSvgRegex = /src="[^\s]+assets[^\s]+(svg)"/g;
+  const srcsetRegex = /srcset="[^"]+"/g;
 
   const destination = './dist/ci/offline/';
 
@@ -23,12 +44,48 @@ gulp.task(task.name, () => {
       console.log('Changed paths in file: ', chunk.path);
       cb(null, chunk);
     }))
-    .pipe(strReplace(regxSVG, () => `assets/media/svgsprite/base${gulpUtil.env.revision}.svg`))
-    .pipe(strReplace(regxMainMin, () => `assets/css/main${gulpUtil.env.revision}.min.css`))
-    .pipe(strReplace(regxFontsMin, () => `assets/css/fonts${gulpUtil.env.revision}.min.css`))
-    .pipe(strReplace(regxPrintMin, () => `assets/css/print${gulpUtil.env.revision}.min.css`))
-    .pipe(strReplace(regxMainJSMin, () => `assets/js/main${gulpUtil.env.revision}.min.js`))
-    .pipe(strReplace(regxHeadJSMin, () => `assets/js/head${gulpUtil.env.revision}.min.js`))
+    .pipe(strReplace(linkRegex, (match) => {
+      const target = match.split('"')[0];
+      const url = match.split('"')[1];
+      const mimeType = url.split('.').pop();
+      const base64Header = mimeTypes[mimeType];
+      const fileContent = Buffer.from(loadFileFromLocationSync(url));
+      if (base64Header) {
+        return `${target}"${base64Header}${fileContent.toString('base64')}"`;
+      }
+      return match;
+    }))
+    .pipe(strReplace(srcsetRegex, () => {
+      return '';
+    }))
+    .pipe(strReplace(cssRegex, (match) => {
+      const url = match.split('"')[3];
+      const fileContent = loadFileFromLocationSync(url, 'utf-8');
+
+      return `<style type="text/css">${fileContent}</style>`;
+    }))
+    .pipe(strReplace(svgRegex, (match) => {
+      const url = match.split('&quot;')[1];
+      return loadFileFromLocationSync(url, 'utf-8');
+    }))
+    .pipe(strReplace(jsRegex, (match) => {
+      const params = match.match(/<script[^<]+>/g)[0].match(/[^\s<]+([^\s>]+|="[^"]+")/g).filter(param => param.indexOf('src=') < 0);
+      const url = match.split('"')[1];
+      const fileContent = loadFileFromLocationSync(url, 'utf-8');
+      return `<${params.join(' ')}>${fileContent}</${params[0]}>`;
+    }))
+    .pipe(strReplace(imgSvgRegex, (match) => {
+      const url = match.split('"')[1];
+      const header = 'data:image/svg+xml;utf8,';
+      const fileContent = loadFileFromLocationSync(url, 'utf-8');
+      return `src="${header}${encodeURIComponent(fileContent)}"'"`;
+    }))
+    .pipe(strReplace(manifestRegex, (match) => {
+      const url = match.split('"')[3];
+      const header = 'data:application/manifest+json,';
+      const fileContent = loadFileFromLocationSync(url, 'utf-8');
+      return `rel="manifest" href='${header}${fileContent.replace(/\r?\n|\r/g, '')}'`;
+    }))
     .pipe(gulpRename('index.html'))
     .pipe(gulp.dest(destination));
 });
