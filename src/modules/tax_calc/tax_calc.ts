6f5d515a8ddcb610dc142bc1496bb4f6a26af4e8
Robert Wüest <robert.wueest@one-inside.com>
2020-02-17 10:59:43 +0100

    Fix regex patterns Add jump function on calc Optimize edit button on tax calc

--- src/modules/tax_calc/tax_calc.ts
+++ src/modules/tax_calc/tax_calc.ts
@@ -380,15 +380,15 @@ class TaxCalc extends Module {
 
   private initStickyEditBtn(): void {
     const toFormBtn = document.querySelector<HTMLElement>('.mdl-tax_calc__toformbtn');
-    const toFormBtnAnker = document.querySelector<HTMLElement>('.mdl-tax_calc__toformbtn_anker');
 
     WindowEventListener.addEventListener('scroll', () => {
-      const rectheight = toFormBtn.getBoundingClientRect().height;
-      const ankerBt = toFormBtnAnker.getBoundingClientRect().bottom;
-      if (ankerBt > window.innerHeight) {
-        toFormBtn.style.top = `${ankerBt - rectheight}px`;
+      const resultBlock = this.ui.resultBlock.getBoundingClientRect();
+      const topMargin = 80;
+
+      if (resultBlock.top < topMargin && resultBlock.bottom > 0) {
+        toFormBtn.style.top = `${topMargin - resultBlock.top}px`;
       } else {
-        toFormBtn.style.top = null;
+        toFormBtn.style.removeProperty('top');
       }
     });
   }
@@ -520,6 +520,7 @@ class TaxCalc extends Module {
 
   private setResultBlocks(blocksProps: TableBlockProperties[], remarks: string[]): void {
     this.ui.resultContainer.innerHTML = '';
+    jump(this.options.domSelectors.resultBlock);
 
     blocksProps.forEach((props) => {
       const newItem = document.createElement('div');
