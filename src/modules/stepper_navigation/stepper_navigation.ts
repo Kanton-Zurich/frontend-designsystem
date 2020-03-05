8c5c77c79eeac4c91ae7e28963b6c7f459dcf68c
Esther Brunner <esther.brunner@zeix.com>
2020-03-05 10:30:19 +0100

    CZHDEV-1752 better use aria-pressed than aria-selected on buttons

--- src/modules/stepper_navigation/stepper_navigation.ts
+++ src/modules/stepper_navigation/stepper_navigation.ts
@@ -143,18 +143,18 @@ class StepperNavigation extends Module {
         this.data.steps[after].removeAttribute('data-visited');
 
         this.ui.step[c].setAttribute('disabled', 'disabled');
-        this.ui.step[c].setAttribute('aria-selected', 'false');
+        this.ui.step[c].setAttribute('aria-pressed', 'false');
       }
     } else if (before !== null) {
       this.ui.step[before].classList.remove(this.options.stateClasses.activeStep);
-      this.ui.step[before].setAttribute('aria-selected', 'false');
+      this.ui.step[before].setAttribute('aria-pressed', 'false');
     }
 
     this.ui.step[after].classList.add(this.options.stateClasses.activeStep);
     this.ui.step[after].classList.remove(this.options.stateClasses.visitedStep);
 
     this.ui.step[after].removeAttribute('disabled');
-    this.ui.step[after].setAttribute('aria-selected', 'true');
+    this.ui.step[after].setAttribute('aria-pressed', 'true');
 
     this.data.steps[after].setAttribute('data-visited', 'true');
 
