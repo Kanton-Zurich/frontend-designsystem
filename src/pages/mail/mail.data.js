e9a1757f2fc26df1ca1ff5393f0f8d5c9ae578f1
Esther Brunner <esther.brunner@zeix.com>
2020-03-05 14:51:16 +0100

    CZHDEV-1349 update notification mail template according to discussion in mini-review

--- src/pages/mail/mail.data.js
+++ src/pages/mail/mail.data.js
@@ -10,7 +10,7 @@ const data = _.merge({}, defaultData, {
     documentation: dataHelper.getDocumentation('mail.md'),
   },
   props: {
-    title: 'Herr Mustermann, Ihre Angaben wurden an das Strassenverkehrsamt übermittelt. Wir werden sie schnellst möglich bearbeiten.',
+    title: 'Ihre Angaben wurden an das Strassenverkehrsamt übermittelt. Wir werden sie schnellst möglich bearbeiten.',
     needtoknow: {
       subtitle: 'Was Sie nun wissen müssen',
       paragraph: 'Wir werden Ihnen den internationalen Führerausweis per Post zustellen. Sobald Sie ihn erhalten haben, müssen Sie folgendes beachten:',
@@ -28,61 +28,76 @@ const data = _.merge({}, defaultData, {
         text: '+41 44 489 45 89',
       },
     },
-    userdata: {
-      subtitle: 'Ihre Angaben',
-      properties: [
-        {
-          label: 'Vorname',
-          value: 'Hans',
-        },
-        {
-          label: 'Nachname',
-          value: 'Mustermann',
-        },
-        {
-          label: 'Geburtsdatum',
-          value: '21.02.1970',
-        },
-        {
-          label: 'E-Mail',
-          value: 'hans.mustermann@zh.ch',
-        },
-        {
-          label: 'Kind',
-          block: [
-            {
-              label: 'Vorname',
-              value: 'Hansi',
-            },
-            {
-              label: 'Nachname',
-              value: 'Musterkind',
-            },
-            {
-              label: 'Geburtsdatum',
-              value: '21.02.2010',
-            },
-          ],
-        },
-        {
-          label: 'Weiteres Kind',
-          block: [
-            {
-              label: 'Vorname',
-              value: 'Martina',
-            },
-            {
-              label: 'Nachname',
-              value: 'Musterkind',
-            },
-            {
-              label: 'Geburtsdatum',
-              value: '21.02.2012',
-            },
-          ],
-        },
-      ],
-    },
+    userdata: [
+      {
+        subtitle: 'Persönliche Angaben',
+        properties: [
+          {
+            label: 'Vorname',
+            value: 'Hans',
+          },
+          {
+            label: 'Nachname',
+            value: 'Mustermann',
+          },
+          {
+            label: 'Geburtsdatum',
+            value: '21.02.1970',
+          },
+          {
+            label: 'E-Mail',
+            value: 'hans.mustermann@zh.ch',
+          },
+          {
+            label: 'Kind',
+            block: [
+              {
+                label: 'Vorname',
+                value: 'Hansi',
+              },
+              {
+                label: 'Nachname',
+                value: 'Musterkind',
+              },
+              {
+                label: 'Geburtsdatum',
+                value: '21.02.2010',
+              },
+            ],
+          },
+          {
+            label: 'Weiteres Kind',
+            block: [
+              {
+                label: 'Vorname',
+                value: 'Martina',
+              },
+              {
+                label: 'Nachname',
+                value: 'Musterkind',
+              },
+              {
+                label: 'Geburtsdatum',
+                value: '21.02.2012',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        subtitle: 'Berufliche Informationen',
+        properties: [
+          {
+            label: 'Firma',
+            value: 'Zeix AG',
+          },
+          {
+            label: 'Berufsbezeichnung',
+            value: 'UX Architect',
+          },
+        ],
+      },
+    ],
   },
 });
 
