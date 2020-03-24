388820738cc1d500b246733a95b156613d7e76b9
Robert Wüest <robert.wueest@one-inside.com>
2020-03-24 14:17:04 +0100

    Biometrie app fixes
    Search field fixes
    
    Stepper form fixes
    
    Banner bugfix
    
    Add Inxmail assets
    
    CZHDEV-2091
    
    CZHDEV-2297
    
    CZHDEV-2298
    
    CZHDEV-2299
    
    CZHDEV-2331
    
    CZHDEV-2359

--- src/modules/banner/banner.ts
+++ src/modules/banner/banner.ts
@@ -101,11 +101,16 @@ class Banner extends Module {
 
   initBanner() {
     if (this.supportsLocalStorage() && localStorage.getItem('closedBanners')) {
-      const uid = this.ui.element.querySelector('[data-uid]').getAttribute('data-uid');
-      this.data.closedItems = JSON.parse(localStorage.getItem('closedBanners'));
-      if (this.data.closedItems.indexOf(uid) >= 0) {
-        this.destroy();
-        this.ui.element.remove();
+      const uidElement = this.ui.element.querySelector('[data-uid]').getAttribute('data-uid');
+      if (uidElement) {
+        const uid = uidElement.getAttribute('data-uid');
+        this.data.closedItems = JSON.parse(localStorage.getItem('closedBanners'));
+        if (this.data.closedItems.indexOf(uid) >= 0) {
+          this.destroy();
+          this.ui.element.remove();
+          return;
+        }
+      } else {
         return;
       }
     }
