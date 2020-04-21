a42214b7b0874908bacd682082876d7bfcf96a8c
Robert Wüest <robert.wueest@one-inside.com>
2020-04-21 16:38:24 +0200

    Fix various bugs:
    - CZHDEV-2443
    
    - CZHDEV-2431
    
    - CZHDEV-2432
    
    - CZHDEV-2434
    
    - CZHDEV-2436
    
    - CZHDEV-2437

--- src/modules/biometrie_appointment/biometrie_appointment_page.mock.data.js
+++ src/modules/biometrie_appointment/biometrie_appointment_page.mock.data.js
@@ -1,11 +1,11 @@
-const biometrieData = require('./biometrie_appointment.data.js');
+const biometrieData = require('../instructions/instructions.data.js');
 const dataHelper = require('@unic/estatico-data');
 
 dataHelper.getFileContent('biometrie_appointment_page.mock.hbs');
 
 const data = {
   canonical: 'https://ma.zh.ch/internet/sicherheitsdirektion/migrationsamt/de/einreise_aufenthalt/ausweise_bewilligungsarten/biometrie.html',
-  biometrieData: biometrieData.variants.default.props,
+  biometrieData: biometrieData.variants.biometrie.props,
   pageHeader: {
     pageTitle: 'Termin verschieben',
     inverted: true,
