e2408ceb4889074a11a19e6902d5ffb95d87a3ce
Marcel Bührig <marcel.buehrig@zeix.com>
2020-05-11 08:42:50 +0200

    More checking of negative linking of rules

--- src/assets/js/helpers/formrules.class.ts
+++ src/assets/js/helpers/formrules.class.ts
@@ -128,7 +128,9 @@ class FormRules {
           const parentRules = JSON.parse(closestParent.dataset.rules);
 
           parentRules.forEach((parentRule, c) => {
-            if (parentRule.action === 'hide' && rule.action === 'disable') {
+            if ((parentRule.action === 'hide' && rule.action === 'disable')
+            || (parentRule.action === 'hide' && rule.action === 'enable')
+            || (parentRule.action === 'hide' && rule.action === 'show')) {
               for (let i = 0; i < parentRule.conditions; i += 1) {
                 parentRule.conditions[i].equals = !parentRule.conditions[i].equals;
               }
