b7fb6db9159774ddcb65865a0b8f55bd78dca234
Esther Brunner <esther.brunner@zeix.com>
2020-10-13 21:38:53 +0200

    CZHDEV-2716 add proper focus and keyboard handling to city select list

--- src/assets/js/helpers/zipCity.ts
+++ src/assets/js/helpers/zipCity.ts
@@ -20,6 +20,25 @@ class ZipCity {
 
   private scrollbarInitialized: Boolean;
 
+  private clickListener = () => {
+    this.openOptionList();
+  }
+
+  private keydownListener = (event: KeyboardEvent) => {
+    if (event.key === 'Enter' || event.key === 'Spacebar' || event.key === ' ') {
+      event.stopPropagation();
+      event.preventDefault();
+
+      this.openOptionList();
+    } else if ((event.key === 'Tab') && !event.shiftKey) {
+      this.closeOptionList();
+    }
+  }
+
+  private mouseupListener = () => {
+    this.closeOptionList();
+  }
+
   constructor($zipField, $cityField) {
     this.fields = {
       zip: $zipField,
@@ -43,32 +62,42 @@ class ZipCity {
 
   setWatcher() {
     wrist.watch(this.fields.zip, 'value', debounce((propName, oldVal, newVal) => {
-      if (oldVal !== newVal && /^[1-9][0-9]{3}$/.test(newVal)) {
-        if (typeof this.data[newVal] === 'string') {
-          this.fields.city.value = this.data[newVal];
-        } else if (Array.isArray(this.data[newVal])) {
-          this.fields.cityOptionsList.innerHTML = '';
+      if (oldVal !== newVal) {
+        this.fields.cityOptionsList.innerHTML = '';
 
-          let listItems = '';
+        if (/^[1-9][0-9]{3}$/.test(newVal)) {
+          this.fields.city.readOnly = true;
 
-          this.data[newVal].forEach((city) => {
-            const compiled = template(this.fields.cityOptionsTemplate.innerHTML);
-            const html = compiled({ city });
+          if (typeof this.data[newVal] === 'string') {
+            this.setValue(this.data[newVal]);
+          } else if (Array.isArray(this.data[newVal])) {
+            let listItems = '';
 
-            listItems = `${listItems}${html}`;
-          });
+            this.data[newVal].forEach((city) => {
+              const compiled = template(this.fields.cityOptionsTemplate.innerHTML);
+              const html = compiled({ city });
 
-          this.fields.cityOptionsList.innerHTML = listItems;
+              listItems = `${listItems}${html}`;
+            });
 
-          this.fields.city.parentElement.classList.add(this.options.showOptionsClass);
+            this.fields.cityOptionsList.innerHTML = listItems;
 
-          if (!this.scrollbarInitialized) {
-            SimpleScrollbar.initEl(this.fields.cityOptionsList);
-            this.scrollbarInitialized = true;
+            this.fields.city.parentElement.classList.add(this.options.showOptionsClass);
+
+            this.fields.city.addEventListener('click', this.clickListener);
+            this.fields.city.addEventListener('keydown', this.keydownListener);
+
+            if (!this.scrollbarInitialized) {
+              SimpleScrollbar.initEl(this.fields.cityOptionsList);
+              this.scrollbarInitialized = true;
+            }
+
+            this.addEventListeners();
           }
-
-
-          this.addEventListeners();
+        } else {
+          this.fields.city.readOnly = false;
+          this.unsetValue();
+          this.removeEventListeners();
         }
       }
     }, this.options.debounce));
@@ -77,13 +106,70 @@ class ZipCity {
   addEventListeners() {
     const buttons = this.fields.cityOptionsList.querySelectorAll('button');
 
+    this.fields.city.addEventListener('click', this.clickListener);
+    this.fields.city.addEventListener('keydown', this.keydownListener);
+
+    window.addEventListener('mouseup', this.mouseupListener);
+
     buttons.forEach((button) => {
       button.addEventListener('click', () => {
-        this.fields.city.value = button.getAttribute('value');
+        this.setValue(button.getAttribute('value'));
+      });
+      button.addEventListener('keydown', (event: KeyboardEvent) => {
+        if (event.key === 'Esc' || event.key === 'Escape') {
+          this.fields.city.parentElement.classList.remove(this.options.showOptionsClass);
 
-        this.fields.city.parentElement.classList.remove(this.options.showOptionsClass);
+          this.fields.city.focus();
+          (<any>window).estatico.flyingFocus.doFocusOnTarget(document.activeElement);
+        }
       });
     });
+  }
+
+  removeEventListeners() {
+    this.fields.city.removeEventListener('click', this.clickListener);
+    this.fields.city.removeEventListener('keydown', this.keydownListener);
+  }
+
+  openOptionList() {
+    this.fields.city.parentElement.classList.add(this.options.showOptionsClass);
+
+    const listitems = this.fields.cityOptionsList.querySelectorAll('li');
+
+    listitems.forEach((listitem) => {
+      listitem.classList.remove('selected');
+    });
+
+    window.addEventListener('mouseup', this.mouseupListener);
+
+    const button: HTMLButtonElement = this.fields.cityOptionsList.querySelector(`button[value="${this.fields.city.value}"]`);
+
+    button.parentElement.classList.add('selected');
+    button.focus();
+    (<any>window).estatico.flyingFocus.doFocusOnTarget(document.activeElement);
+  }
+
+  closeOptionList() {
+    window.removeEventListener('mouseup', this.mouseupListener);
+    this.fields.city.parentElement.classList.remove(this.options.showOptionsClass);
+  }
+
+  setValue(value: string) {
+    this.fields.city.value = value;
+
+    this.fields.city.parentElement.classList.remove(this.options.showOptionsClass);
+
+    // show options again on click
+    if (this.fields.cityOptionsList.childElementCount > 0) {
+      this.fields.city.focus();
+      (<any>window).estatico.flyingFocus.doFocusOnTarget(document.activeElement);
+    }
+  }
+
+  unsetValue() {
+    this.fields.city.value = '';
+
+    this.fields.city.parentElement.classList.remove(this.options.showOptionsClass);
   }
 }
 
