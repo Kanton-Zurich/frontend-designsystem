9992fb7c7866f4e33a5735997290f7f7d788f024
Robert Wüest <robert.wueest@one-inside.com>
2021-03-16 11:36:49 +0100

    Add example for form rules (alternative selection)

--- src/modules/form/form.data.js
+++ src/modules/form/form.data.js
@@ -688,6 +688,72 @@ const personType2 = {
   ],
 };
 
+const selectionAlternative = {
+  rows: [
+    {
+      fields: [
+        {
+          cellContent: () => handlebars.compile(selectHBS)({
+            isSingleSelect: true,
+            listData: _.assign(_.merge({}, listDemoData.variants.defaultSingle.props, {
+              setHiddenIndex: true,
+              groupId: 'nationality-x3',
+            }), {
+              selectOptions: [
+                { value: 'schweiz', label: 'Schweiz' },
+                { value: 'lichtenstein', label: 'Lichtenstein' },
+                { value: 'andere', label: 'Andere' },
+              ],
+            }),
+            triggerInputData: {
+              type: 'text',
+              isSelectTrigger: true,
+              isFloatingLabel: true,
+              isInput: false,
+              icon: 'angle_drop_down',
+              label: 'Staatsangehörigkeit',
+              validation: {
+                isRequired: true,
+              },
+            },
+          }),
+        },
+      ],
+    },
+    {
+      fields: [
+        {
+          cellContent: () => handlebars.compile(formInputHBS)(_.merge({},
+            formInputData.variants.default.props,
+            {
+              isFloatingLabel: true,
+              label: 'Andere Nationalität',
+              name: 'other_nationality',
+              uuid: 'other_nationality',
+              type: 'text',
+              validation: {
+                isRequired: true,
+                errorMsg: 'Bitte geben Sie ihre Nationalität ein',
+              },
+              rules: JSON.stringify([
+                {
+                  conditions: [
+                    {
+                      field: 'nationality-x3',
+                      equals: true,
+                      value: 'andere',
+                    },
+                  ],
+                  action: 'show',
+                },
+              ]),
+            })),
+        },
+      ],
+    },
+  ],
+};
+
 const template = dataHelper.getFileContent('form.hbs');
 const data = _.merge({}, defaultData, {
   meta: {
@@ -2685,6 +2751,17 @@ const variants = _.mapValues({
     props: {
       groups: [
         personType2,
+      ],
+    },
+  },
+  alternativeSelection: {
+    meta: {
+      title: 'Auswahlalternative',
+      desc: 'Eine alternative Eingabe zur Auswahl tätigen'
+    },
+    props: {
+      groups: [
+        selectionAlternative,
       ],
     },
   },
