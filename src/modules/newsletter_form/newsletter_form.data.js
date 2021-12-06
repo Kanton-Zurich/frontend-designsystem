1623bd4cefa7e50eb8e33e4d14c17c773060a8e9
Robert Wüest <robert.wueest@one-inside.com>
2021-12-06 13:36:06 +0100

    CZHDEV-2839 adjust newsletter form

--- src/modules/newsletter_form/newsletter_form.data.js
+++ src/modules/newsletter_form/newsletter_form.data.js
@@ -2,7 +2,8 @@ const _ = require('lodash');
 const dataHelper = require('@unic/estatico-data');
 const { handlebars } = require('@unic/estatico-handlebars');
 const defaultData = require('../../data/default.data.js');
-const defFormInputData = require('../../atoms/form_input/form_input.data').variants.floatValidateEmail.props;
+const defFormInputData = require('../../atoms/form_input/form_input.data').variants.defaultForForms.props;
+const defFormInputEmailData = require('../../atoms/form_input/form_input.data').variants.floatValidateEmail.props;
 const defButtonData = require('../../atoms/button/button.data').variants.default.props;
 
 const template = dataHelper.getFileContent('newsletter_form.hbs');
@@ -17,7 +18,7 @@ const data = _.merge({}, defaultData, {
   props: {
     action: '/go_to_newsletter',
     method: 'GET',
-    formInputData: _.merge({}, defFormInputData, {
+    formInputData: _.merge({}, defFormInputEmailData, {
       isSmall: true,
       isFloatingLabel: false,
       label: 'Ihre E-Mail',
@@ -36,9 +37,49 @@ const variants = _.mapValues({
     },
     props: {
       wrapped: true,
+      reduced: true,
       headingLevel: 2,
       title: 'Unser Newsletter',
-      description: 'Abonnieren Sie unseren Newsletter und erhalten Sie die News,  die für Sie relevant sind.',
+      description: 'Abonnieren Sie unseren Newsletter und erhalten Sie die News, die für Sie relevant sind.',
+    },
+  },
+  regular: {
+    meta: {
+      title: 'Komplex',
+      desc: '',
+    },
+    props: {
+      wrapped: false,
+      headingLevel: 2,
+      title: 'Unser Newsletter',
+      description: 'Abonnieren Sie unseren Newsletter und erhalten Sie die News, die für Sie relevant sind.',
+      formInputFirstname: _.merge({}, defFormInputData, {
+        isSmall: false,
+        isFloatingLabel: false,
+        label: 'Vorname',
+        id: 'firstname',
+        validation: {
+          isRequired: true,
+        },
+      }),
+      formInputLastname: _.merge({}, defFormInputData, {
+        isSmall: false,
+        isFloatingLabel: false,
+        label: 'Nachname',
+        id: 'lastname',
+        validation: {
+          isRequired: true,
+        },
+      }),
+      formInputEmail: _.merge({}, defFormInputEmailData, {
+        isSmall: false,
+        isFloatingLabel: false,
+        label: 'Ihre E-Mail',
+        id: 'email',
+      }),
+      buttonData: _.merge({}, defButtonData, {
+        text: 'Newsletter abonnieren',
+      }),
     },
   },
 }, (variant) => {
